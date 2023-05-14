19eabbf20aa2e9ec9adb4d42e5f68b4d5a766026
Superamaja <38116622+Superamaja@users.noreply.github.com>
2023-05-13 23:39:49 -0500

    Added a button to remove self from submitted set

--- DemoUIs/demoView.py
+++ DemoUIs/demoView.py
@@ -7,10 +7,18 @@ from DemoUIs.demoSelect import Select
 class DemoView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(Select())
+        self.select = Select()
+        self.add_item(self.select)
 
     @discord.ui.button(label="Open Demo Modal", style=discord.ButtonStyle.blurple)
     async def open_demo_modal(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
         await interaction.response.send_modal(Modal())
+
+    @discord.ui.button(label="Remove from submitted list", style=discord.ButtonStyle.red)
+    async def remove_user(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.select.submitted.discard(interaction.user.id)
+        await interaction.response.edit_message(view=self)
